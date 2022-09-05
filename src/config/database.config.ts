41ce082886d5c2dbdf3a54a3bdf9c4529635f16f
Edwin Cosme <adriancosme15@gmail.com>
2022-09-05 18:53:22 -0500

    chore(config): remove user pass keys

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,8 +4,6 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 function mongooseModuleOptions(): MongooseModuleOptions {
   return {
     uri: process.env.MONGO_URI,
-    user: process.env.MONGO_USER,
-    pass: process.env.MONGO_PASS,
     dbName: process.env.MONGODB_NAME,
     authSource: process.env.MONGO_AUTH_SOURCE,
     retryWrites: Boolean(process.env.MONGO_RETRY_WRITES) || false,
