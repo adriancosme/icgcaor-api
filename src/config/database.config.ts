1bf8432d37b2e0a4fbc6b44386ecd64cb2f5db61
Edwin Cosme <adriancosme15@gmail.com>
2022-09-05 19:12:33 -0500

    chore: database configuration

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,10 +2,10 @@ import { registerAs } from '@nestjs/config';
 import { MongooseModuleOptions } from '@nestjs/mongoose';
 
 function mongooseModuleOptions(): MongooseModuleOptions {
+  console.log(process.env.MONGO_URI);
   return {
     uri: process.env.MONGO_URI,
-    dbName: process.env.MONGODB_NAME,
-    authSource: process.env.MONGO_AUTH_SOURCE,
+
     retryWrites: Boolean(process.env.MONGO_RETRY_WRITES) || false,
     useNewUrlParser: true,
   };
