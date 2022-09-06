bed397317af461f1093d26036f167b867c54202b
Edwin Cosme <adriancosme15@gmail.com>
2022-09-05 20:25:33 -0500

    chore: remove unnecesary configurations

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,9 +5,6 @@ function mongooseModuleOptions(): MongooseModuleOptions {
   console.log(process.env.MONGO_URI);
   return {
     uri: process.env.MONGO_URI,
-    authSource: process.env.MONGO_AUTH_SOURCE,
-    retryWrites: Boolean(process.env.MONGO_RETRY_WRITES) || false,
-    useNewUrlParser: true,
   };
 }
 
